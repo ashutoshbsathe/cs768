bcc1181eaaba78d56024faf572da785b6aba55bb
Ashutosh Sathe <2ashutoshbs@gmail.com>
2021-11-23 12:26:26 +0530

    Add scripts for basic experiments

---
+++ all_graphsage.sh
@@ -0,0 +1,15 @@
+#!/bin/sh 
+set -e 
+set -x
+
+python train.py --conv_operator GraphSAGE \
+    --num_layers 2 
+
+python train.py --conv_operator GraphSAGE \
+    --num_layers 4 
+
+python train.py --conv_operator GraphSAGE \
+    --num_layers 8 
+
+python train.py --conv_operator GraphSAGE \
+    --num_layers 16 
